06cc6fc56678b40529d4d279b2c46df3bff0b285
Danil <danila_monster@mail,ru>
2022-08-28 18:07:22 +0300

    DZ52 nedodel

---
+++ DZ_Task_52/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 
+
+
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; ++row)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); ++row)
+    {
+        for (int column = 0; column < array.GetLength(1); ++column)
+        {
+            Console.Write($"{array[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void srAr(int[,] array)
+{
+    
+    int srAr = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int summ = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            summ += array[i, j];
+        }
+        Console.WriteLine($"Сумма элементов {i+1}-й строки равна {summ}");
+    }
+        //Console.WriteLine($"Cреднее арифметическое элементов столбца {j + 1} = {(double)srAr / array.GetLength(1)}");
+}
+
+
+
+
+
+int[,] matrix = CreateMatrix(3, 4, 0, 9);
+PrintMatrix(matrix);
+srAr(matrix);
